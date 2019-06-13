502751e869f13a4152015cf674c2edf06ff4c5e7
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-13 11:25:19 +0000

    refactor: Uses useReducer to handle input changes

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import axios from 'axios';
 import FriendsList from './components/FriendsList';
 import AddFriend from './components/AddFriend';
@@ -9,9 +9,9 @@ function App() {
   const [friends, setFriends] = useState([]);
   const [requestError, setError] = useState('');
   const [loading, setLoading] = useState(false);
-  const [friendName, setFriendName] = useState('');
-  const [friendAge, setFriendAge] = useState(0);
-  const [friendEmail, setFriendEmail] = useState('');
+  const [friend, setUserInput] = useReducer((state, newState) => (
+    { ...state, ...newState }
+  ), { name: '', age: 0, email: '' });
 
   useEffect(() => {
     getAllFriends();
@@ -34,19 +34,7 @@ function App() {
     const eventName = event.target.name;
     const { value } = event.target;
 
-    switch (eventName) {
-      case 'name':
-        setFriendName(value);
-        break;
-      case 'age':
-        setFriendAge(value);
-        break;
-      case 'email':
-        setFriendEmail(value);
-        break;
-      default:
-        break;
-    }
+    setUserInput({ [eventName]: value });
   };
 
   return (
@@ -57,9 +45,9 @@ function App() {
         loading={loading}
       />
       <AddFriend
-        name={friendName}
-        age={friendAge}
-        email={friendEmail}
+        name={friend.name}
+        age={friend.age}
+        email={friend.email}
         addFriendInputHandler={addFriendInputHandler}
       />
     </div>
